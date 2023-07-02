f08a41e6072fbad2cd41117408e5744d64aa2ac7
Zotoff <paulzotoff@gmail.com>
2023-07-02 12:31:25 +0300

    First attempt

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,55 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Profile profile = new Profile();
+
+        String name;
+        int age;
+        String email;
+        long phone;
+
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r"); FileChannel inChannel = aFile.getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            StringBuilder newString = new StringBuilder();
+
+            while (inChannel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); i++) {
+                    newString.append((char) buffer.get());
+                }
+                buffer.clear();
+                inChannel.read(buffer); // reading the buffer
+            }
+
+            String formattedText = newString.toString().replace("\n", ",").replace("\r", "");
+            Map<String, String> map = new HashMap<>();
+            String[] listString = formattedText.split("\\,");
+            for (String element : listString) {
+                String[] field = element.split(": ");
+                map.put(field[0], field[1]);
+            }
+
+            name = map.get("Name");
+            age = Integer.valueOf(map.get("Age"));
+            email = map.get("Email");
+            phone = Long.valueOf(map.get("Phone"));
+
+            profile = new Profile(name, age, email, phone);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
